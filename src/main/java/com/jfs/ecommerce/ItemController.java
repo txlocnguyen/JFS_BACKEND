1bda73bccc0a379cd28a11e0565e7c6f01febc5e
Loc Nguyen <txlocnguyen@gmail.com>
2022-03-23 15:33:17 -0500

    debug

--- src/main/java/com/jfs/ecommerce/ItemController.java
+++ src/main/java/com/jfs/ecommerce/ItemController.java
@@ -38,9 +38,15 @@ public class ItemController
     public Item update( @RequestBody ItemDto itemDto, @PathVariable Integer id )
     {
         Item item = itemService.findById( id );
-        item.setName( itemDto.getName() );
-        item.setDescription( itemDto.getDescription() );
-        item.setImageUrl( itemDto.getImageUrl() );
+        if(itemDto.getName() != null){
+            item.setName( itemDto.getName() );
+        }
+        if(itemDto.getDescription() != null){
+            item.setDescription( itemDto.getDescription() );
+        }
+        if(itemDto.getImageUrl() != null){
+            item.setImageUrl( itemDto.getImageUrl() );
+        }
         return itemService.save( item );
     }
     @CrossOrigin
